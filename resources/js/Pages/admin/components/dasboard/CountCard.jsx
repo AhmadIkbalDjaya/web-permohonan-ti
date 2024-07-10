ae6f51daaf933267a83f79b26a9618598532018f
AhmadIkbalDjaya <ikbaldjaya@gmail.com>
2024-07-10 12:03:30 +0800

    feat: dashboard count card ui

---
+++ resources/js/Pages/admin/components/dasboard/CountCard.jsx
@@ -0,0 +1,81 @@
+import { Avatar, Box, Typography } from "@mui/material";
+import React from "react";
+import { MdArticle } from "react-icons/md";
+export function CountCard({
+    title = "Proposal",
+    count = 0,
+    new_count = 0,
+    icon = <MdArticle size={20} color="#375DFB" />,
+}) {
+    return (
+        <Box
+            p={1}
+            display={"flex"}
+            sx={{
+                background: "white",
+                borderRadius: "5px",
+                boxShadow: 1,
+            }}
+            width={"100%"}
+        >
+            <Box
+                display={"flex"}
+                justifyContent={"space-between"}
+                columnGap={2}
+            >
+                <Box>
+                    <Typography
+                        variant="subtitle2"
+                        color="gray-500"
+                        fontWeight={"600"}
+                        textTransform={"uppercase"}
+                        fontSize={12}
+                    >
+                        {title}
+                    </Typography>
+                    <Typography
+                        variant="h6"
+                        color="black"
+                        display={"flex"}
+                        gap={1}
+                        alignItems="end"
+                        fontWeight={600}
+                    >
+                        {count}
+                        <Typography
+                            variant="body1"
+                            color="inherit"
+                            fontWeight={600}
+                        >
+                            Permohonan
+                        </Typography>
+                    </Typography>
+                    <Typography
+                        variant="subtitle2"
+                        color="gray-500"
+                        fontSize={11}
+                    >
+                        <Typography
+                            variant="inherit"
+                            color="#375DFB"
+                            display={"inline"}
+                        >
+                            {new_count}{" "}
+                        </Typography>
+                        perhomanan baru
+                    </Typography>
+                </Box>
+                <Avatar
+                    sx={{
+                        bgcolor: "#DBE7FF",
+                        width: "35px",
+                        height: "35px",
+                    }}
+                >
+                    {icon}
+                    {/* <MdArticle size={20} color="#375DFB" /> */}
+                </Avatar>
+            </Box>
+        </Box>
+    );
+}
--- resources/js/Pages/admin/dashboard/Index.jsx
+++ resources/js/Pages/admin/dashboard/Index.jsx
@@ -1,14 +1,24 @@
+import { CountCard } from "../components/dasboard/CountCard";
 import React from "react";
 import BaseLayout from "../base_layout/BaseLayout";
 import { Head } from "@inertiajs/react";
-import { Typography } from "@mui/material";
+import { Avatar, Box, Grid, Typography } from "@mui/material";
+import { MdArticle } from "react-icons/md";
 
 export default function Dashboard() {
     return (
         <>
             <Head title="Dashboard" />
             <BaseLayout>
-                <Typography fontSize={24} fontWeight={"bold"}>Dashboard</Typography>
+                <Typography fontSize={24} fontWeight={"bold"}>
+                    Dashboard
+                </Typography>
+                <Box display={"flex"} columnGap={3} justifyContent={"space-between"}>
+                    <CountCard />
+                    <CountCard />
+                    <CountCard />
+                    <CountCard />
+                </Box>
             </BaseLayout>
             ;
         </>
